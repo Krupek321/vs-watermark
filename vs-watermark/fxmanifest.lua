927802a545c9862f08dbaa6a22e499f721ee534f
Krupek321 <99915406+Krupek321@users.noreply.github.com>
2024-04-08 22:10:21 +0200

    Add files via upload

---
+++ vs-watermark/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+version '1.0.0'
+
+author 'Voice Studio'
+description 'Watermark'
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/style.css'
+}
